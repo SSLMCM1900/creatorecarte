f3dcd5c0118e48e7393d886f67e04e580d735ab8
SSLMCM1900 <pascuttinisimone@gmail.com>
2025-08-06 21:05:41 +0200

    Update script.js

--- script.js
+++ script.js
@@ -317,12 +317,12 @@ function updateFooterSymbol(e) {
 
 // Rarità
 const rarityLabel = document.createElement("label");
-rarityLabel.textContent = "Rarità (C, NC, R, UR)";
+rarityLabel.textContent = "Rarità (C, NC, R, SR, UR)";
 rarityLabel.style.display = "block";
 rarityLabel.style.marginTop = "15px";
 
 const raritySelect = document.createElement("select");
-["", "C", "NC", "R", "UR"].forEach(rarity => {
+["", "C", "NC", "R","SR", "UR"].forEach(rarity => {
     const opt = document.createElement("option");
     opt.value = rarity;
     opt.textContent = rarity;
